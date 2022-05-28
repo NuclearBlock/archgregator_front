caaef3942e42581dee08654cc2cb7d14fe15b5bd
user <user@MBP-user.Dlink>
2022-05-28 04:24:59 +0300

    Empty data display fix

--- client/src/components/dashboard/DashboardChart.jsx
+++ client/src/components/dashboard/DashboardChart.jsx
@@ -1,6 +1,5 @@
 import React, { useState, PureComponent } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-//import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Card from '@material-ui/core/Card';
@@ -28,53 +27,6 @@ const useStyles = makeStyles({
   });
 
 
-  const data1 = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-
-
-
 export default function DashdoardChart({data, isLoading}) {
 
     const classes = useStyles();
@@ -83,44 +35,40 @@ export default function DashdoardChart({data, isLoading}) {
     return (
 
       <>
+        <ResponsiveContainer width="100%" height="100%">
+          <Card variant="outlined" square className={classes.card}>
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  REWARDS CHART
+              </Typography>
 
-<ResponsiveContainer width="100%" height="100%">
-    <Card variant="outlined" square className={classes.card}>
-        <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-                REWARDS CHART
-            </Typography>
+              {isLoading && <div className="chart-progress">Loading data ...</div>} 
 
-            {isLoading && <div class="dashboard-progress">Loading data ...</div>} 
-
-            {data.length > 0 &&
-              <AreaChart
-                width={700}
-                height={280}
-                data={data}
-                margin={{
-                  top: 0,
-                  right: 0,
-                  left: -40,
-                  bottom: 0,
-                }}
-              >
-                <CartesianGrid strokeDasharray="5 5" />
-                <XAxis dataKey="date" />
-                <YAxis tick={false} type="number" domain={['auto', 'auto']} />
-                <Tooltip />
-                <Area type="monotone" dataKey="amount" stroke="#ff5820" fill="#f7ab93" />
-              </AreaChart>
-            }
-
-        </CardContent>
-    </Card>
-
-    </ResponsiveContainer>
-
-
-
-</>
+              {data.length == 0 && <div className="chart-progress">No data found</div>}
+              
+              {data.length > 0 &&
+                <AreaChart
+                  width={700}
+                  height={280}
+                  data={data}
+                  margin={{
+                    top: 0,
+                    right: 0,
+                    left: -40,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="5 5" />
+                  <XAxis dataKey="date" />
+                  <YAxis tick={false} type="number" domain={['auto', 'auto']} />
+                  <Tooltip />
+                  <Area type="monotone" dataKey="amount" stroke="#ff5820" fill="#f7ab93" />
+                </AreaChart>
+              }
+            </CardContent>
+          </Card>
+        </ResponsiveContainer>
+      </>
 
 
     );

dbc70c23f1c32794fd6ff7e5de79d964b4aee7b4
user <user@MBP-user.Dlink>
2022-05-28 04:44:23 +0300

    404 pages fix

--- client/src/pages/PageSearchNotFound.jsx
+++ client/src/pages/PageSearchNotFound.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
-export default function Page404() {
+export default function PageSearchNotFound() {
 
-
-    return (
-        
+    return (      
         <>
             <Grid container spacing={0}>
                 <Grid  item xs={12} spacing={0}>
-                    <h1>Page not found</h1>
+                    <h1>No Result found</h1>
                 </Grid>
             </Grid>
 
             <Grid container spacing={0}>
                 <Grid  item xs={12} spacing={0}>
-                    <div class="not-found ">
-                        <p>404</p>
+                    <div className="not-found ">
+                        <p>Not found</p>
                     </div>
                 </Grid>
             </Grid>
 
-        </>
-        
+        </>     
     );
 }
